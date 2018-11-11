27a189b39e50f7cb089788ecc07df7f48eab31af
Raphael <raphaelrdsoares@gmail.com>
2018-11-11 00:32:18 -0300

    implementando os métodos de busca e delete (único e todos);

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,62 @@
  * @version 0.1.0
  * ---------------------------------------------------
  */
+angular.module("app", []).controller("DomainController", [
+	"$scope",
+	"$sce",
+	"$http",
+	function($scope, $sce, $http) {
+		$scope.inpTxt_mainUrl = "";
+		$scope.records = [];
 
-$(document).ready(function () {
-    
-});
+		$scope.onSubmitSearch = function() {
+			refreshSearch();
+		};
+
+		$scope.onDelete = function(id) {
+			var recordToDelete = undefined;
+			$scope.records.forEach(element => {
+				if (element.id === id) recordToDelete = element;
+			});
+			if (recordToDelete) deleteRecords([recordToDelete]);
+		};
+
+		$scope.onDeleteAll = function() {
+			deleteRecords($scope.records);
+		};
+
+		function deleteRecords(records) {
+			records.forEach(element => {
+				element._metadata["changeTrack"] = "destroy";
+			});
+
+			var persistURL = $scope.inpTxt_mainUrl.substr(0, $scope.inpTxt_mainUrl.lastIndexOf("/")) + "/persist";
+			$http({
+				method: "POST",
+				url: persistURL,
+				data: records
+			}).then(function() {
+				//success
+				refreshSearch();
+			});
+		}
+
+		function refreshSearch() {
+			$http.get($scope.inpTxt_mainUrl).then(function(response) {
+				$scope.records = response.data;
+				console.log($scope.records);
+			});
+		}
+	}
+]);
+
+// $(document).ready(function() {
+// 	$("#form_searchByURL").submit(function(e) {
+// 		e.preventDefault();
+// 		var mainUrl = $("#inpTxt_mainUrl").val();
+
+// 		$.get(mainUrl, function(data, textStatus, jqXHR) {
+// 			console.log(data);
+// 		});
+// 	});
+// });
