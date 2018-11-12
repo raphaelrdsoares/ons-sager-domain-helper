87c6cd66648211ff1e04d200307442fd5fc2cd5a
Raphael <raphaelrdsoares@gmail.com>
2018-11-11 21:11:08 -0300

    modal de confirmação de exclusão; edição de registros;

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ons-sager-domain-helper
+
+Baixar o projeto.
+Executar \$ npm install
+executar o gulp build
+Pre-requisito: instalar no chrome a extensão [CORS][https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en]
+rodar o index.html na pasta /dist
--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,21 @@
  * @version 0.1.0
  * ---------------------------------------------------
  */
-//TODO: Colocar pop de confirmação na exclusão (enter pra confirmar e esc para cancelar)
+//OK: Opção de expandar o registro pra ver seus dados
+//OK: Colocar pop de confirmação na exclusão (enter pra confirmar e esc para cancelar)
+//InTest: Opção de editar os dados de cada registro
+
 //TODO: Opção de salvar a URL (com um alias) + exibir os alias das URLs salvar em cima pra facilitar. Ao clicar no link do alias, carregar a URL no input e já realizar a busca
-//TODO: Opção de expandar o registro pra ver seus dados
-//TODO: Opção de editar os dados de cada registro
+//TODO: Opção de buscar por todos os tipos. Ao exibir os registros, caso sejam de tipos diferentes, exibir o tipo junto com o id. Lembrar de verificar a url de persistência.
+//TODO: Subir a pasta /dist para o git e testar a execução usando apenas esta pasta num folder separado (desconfio que o font awesome não vai funcionar. Ajustar o build pra trazer o font awesome também)
 
 angular.module("app", []).controller("DomainController", [
 	"$scope",
 	"$sce",
 	"$http",
 	function($scope, $sce, $http) {
-		$scope.inpTxt_mainUrl = "";
+		$scope.inpTxt_mainUrl = "http://nl4zj.mocklab.io/sager";
+		$scope.persistURL = "";
 		$scope.records = [];
 
 		$scope.onSubmitSearch = function() {
@@ -37,15 +41,58 @@ angular.module("app", []).controller("DomainController", [
 			deleteRecords($scope.records);
 		};
 
+		$scope.onEditRecord = function(id) {
+			var recordToEdit = undefined;
+			$scope.records.forEach(element => {
+				if (element.id === id) {
+					recordToEdit = JSON.parse(JSON.stringify(element));
+					delete recordToEdit["_metadata"];
+					delete recordToEdit["id"];
+					delete recordToEdit["$$hashKey"];
+					$("#txtArea_editRecord").val(JSON.stringify(recordToEdit, undefined, 4));
+				}
+			});
+		};
+
+		$scope.onSaveEditRecord = function(id) {
+			var obj = JSON.parse($("#txtArea_editRecord").val());
+			$scope.records.forEach(element => {
+				if (element.id === id) {
+					obj["id"] = id;
+					obj["_metadata"] = element._metadata;
+
+					updateRecord(obj);
+				}
+			});
+		};
+
+		$scope.format = function(object) {
+			var clone = JSON.parse(JSON.stringify(object));
+			delete clone["_metadata"];
+			return clone;
+		};
+
+		function updateRecord(record) {
+			record._metadata["changeTrack"] = "update";
+
+			$http({
+				method: "POST",
+				url: $scope.persistURL,
+				data: [record]
+			}).then(function() {
+				//success
+				refreshSearch();
+			});
+		}
+
 		function deleteRecords(records) {
 			records.forEach(element => {
 				element._metadata["changeTrack"] = "destroy";
 			});
 
-			var persistURL = $scope.inpTxt_mainUrl.substr(0, $scope.inpTxt_mainUrl.lastIndexOf("/")) + "/persist";
 			$http({
 				method: "POST",
-				url: persistURL,
+				url: $scope.persistURL,
 				data: records
 			}).then(function() {
 				//success
@@ -56,6 +103,8 @@ angular.module("app", []).controller("DomainController", [
 		function refreshSearch() {
 			$http.get($scope.inpTxt_mainUrl).then(function(response) {
 				$scope.records = response.data;
+				$scope.persistURL =
+					$scope.inpTxt_mainUrl.substr(0, $scope.inpTxt_mainUrl.lastIndexOf("/")) + "/persist";
 				console.log($scope.records);
 			});
 		}
